b324cfc2714bb08352be5caedefbfa479c7aa80c
KEO <keoxcoelho@gmail.com>
2025-05-09 07:46:44 -0300

    v3

---
+++ GpsScanDotnet/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GpsScanDotnet.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
